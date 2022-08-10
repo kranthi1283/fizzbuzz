743e70e05b8a760a29d26888aa80cfaede40feba
kranthi <kranthi1283@gmail.com>
2022-08-10 18:16:31 -0500

    Initial Commit

---
+++ Client/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FizzBuzz
+{
+    class ConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            int i = 0;
+            Console.WriteLine("Enter Upper Bound number");
+            Int32.TryParse(Console.ReadLine(), out i);
+            //c.Output(i);
+            c.Output(int.MaxValue); // Leaving this to Max Value. We can uncomment line 11 and comment this line to check upper bound entry
+
+        }
+    }
+}
---
+++ FizzBuzz/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace FizzBuzz
+{
+    public class Class1
+    {
+        // Using the dictionary to store the combinations we are entering
+        public Dictionary<int, string> keyValue = new Dictionary<int, string>();
+
+       
+        /// <summary>
+        /// Output Method
+        /// <paramref name="upperBound"/>
+        /// </summary>
+        /// <param name="upperBound"></param>
+        public void Output(int upperBound)  // This method outputs the whole string based on the combinations and Maximum Upper Bound value
+        {
+
+            UserInput();
+            
+            for (int i = 0; i < upperBound; i++)
+            {
+                string output = string.Empty;
+                foreach (var comb in keyValue)
+                {
+                    if (i % comb.Key ==0)
+                        output += comb.Value;
+                }
+                if(output == null)
+                {
+                    output = i.ToString();
+                }
+                Console.WriteLine (output);
+            }
+        }
+
+        
+        /// <summary>
+        /// User Input Method
+        /// </summary>
+        public void UserInput() //This method takes input from user for all the combinations. "Exit" when done.
+        {
+            Console.WriteLine("Please enter integer and string combinations with a space. Enter Exit when done");
+            string line;
+            do
+            {
+                line = Console.ReadLine();
+                string[] parts = line.Split(' ');
+                Int32.TryParse(parts[0], out int number);
+                if (parts.Length == 2 && number != 0)
+                    keyValue.Add(number, parts[1]);
+
+            } while (line != null && !line.ToLower().Equals("exit"));
+        }
+    }
+}
